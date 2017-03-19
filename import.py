545c82c92616339393902dde32ea8c5cb77ae597
Rui Carmo <rui.carmo@gmail.com>
2017-03-19 00:22:23 +0000

    DocumentDB-safe but human-friendly IDs

--- import.py
+++ import.py
@@ -3,13 +3,25 @@
 """OPML importer"""
 
 from time import time, sleep, strftime
+from base64 import urlsafe_b64encode
+from urllib.parse import urlparse
+from hashlib import sha256
 from asyncio import get_event_loop, Semaphore, gather, ensure_future
 from aiohttp import ClientSession
 from xml.etree import ElementTree
 
 from pymongo import MongoClient, ASCENDING, DESCENDING
+from pymongo.errors import DuplicateKeyError
 from datetime import datetime
 from config import log, FETCH_INTERVAL, MONGO_SERVER, DATABASE_NAME, DATE_FORMAT
+
+
+def safe_id(url):
+    fragments = urlparse(url)
+    safe = fragments.netloc + fragments.path.replace('/', '_').replace('+', '-')
+    if fragments.params or fragments.query:
+        safe += sha256(bytes(url,'utf-8')).hexdigest()
+    return safe
 
 
 def feeds_from_opml(filename):
@@ -34,9 +46,12 @@ def update_database(db, filename):
     for feed in feeds_from_opml(filename):
         if not feeds.find_one({'url': feed['url']}):
             log.debug("Inserting %s" % feed)
-            #feed['_id'] = feed['url']
+            feed['_id'] = safe_id(feed['url'])
             feed['created'] = datetime.now()
-            feeds.insert_one(feed)
+            try:
+                feeds.insert_one(feed)
+            except DuplicateKeyError as e:
+                log.debug(e)
 
 
 if __name__ == '__main__':
